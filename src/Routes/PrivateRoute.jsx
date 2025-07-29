d0e595c3040b9ea8cb67113bb2d6cb4c78bf4749
Atik Hassan <atikh01001@gmail.com>
2025-07-29 17:41:08 +0600

    private route created

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,23 +1,25 @@
-import React, { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router';
-import { AuthContext } from '../contexts/AuthContext';
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from "../contexts/AuthContext";
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  // console.log(location);
 
-    const {user,loading} =useContext(AuthContext)
-    const location = useLocation()
-    // console.log(location);
-    
+  if (loading) {
+    return (
+      <div className="text-center">
+        <span className="loading loading-spinner loading-xl"></span>
+      </div>
+    );
+  }
 
-    if (loading) {
-        return <div className='text-center'><span className="loading loading-spinner loading-xl"></span></div>
-    }
+  if (!user) {
+    return <Navigate state={location?.pathname} to="/login"></Navigate>;
+  }
 
-    if (!user) {
-        return <Navigate state={location?.pathname} to="/login"></Navigate>
-    }
-
-    return children
+  return children;
 };
 
 export default PrivateRoute;
